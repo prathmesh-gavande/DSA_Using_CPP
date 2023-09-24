57a957a714dfcc833d60d9012bd9d455c396d23a
Prathmesh <prathmeshgavande117@gmail.com>
2023-09-24 08:09:03 +0530

    pattern printing

--- CPP basics/loops.cpp
+++ CPP basics/loops.cpp
@@ -13,28 +13,28 @@ int main() {
 			break;
 		}
 		
+	// }
+
+	// cout << "Hi" << endl;
+
+	
+	for(int i=0; i<3; i=i+1) {
+		cout<< endl << "Outer Loop " << i << endl << endl; 
+		
+		for(int j=0; j<3; j=j+1) {
+			cout << "inner loop " << j << endl;
+		}
+		
 	}
-
-	cout << "Hi" << endl;
-
 	
-	// for(int i=0; i<3; i=i+1) {
-	// 	cout<< endl << "Outer Loop " << i << endl << endl; 
-		
-	// 	for(int j=0; j<3; j=j+1) {
-	// 		cout << "inner loop " << j << endl;
-	// 	}
-		
-	// }
+	for(int i=0; i<5; i=i+1) {
+		cout << "A" << " ";
+	}
+	cout << endl;
 	
-	// for(int i=0; i<5; i=i+1) {
-	// 	cout << "A" << " ";
-	// }
-	// cout << endl;
-	
-	// for(int i=10 ;i>=0; i= i - 3) {
-	// 	cout << i << endl;
-	// }
+	for(int i=10 ;i>=0; i= i - 3) {
+		cout << i << endl;
+	}
 	
 	// for(int i=2; i<10; i=i+2) {
 	// 	cout << i << endl;
